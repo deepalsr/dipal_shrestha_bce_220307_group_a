60f596df86194edf4501e2e0a7d37f7a9b733725
deepalsr <deepalshrt@gmail.com>
2024-05-29 17:35:32 +0545

    Lab 3 programs

---
+++ GUI.java
@@ -0,0 +1,12 @@
+
+import java.awt.Frame;
+
+public class GUI {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setSize(400,400 );
+        frame.setVisible(true);
+    }
+
+    
+}
